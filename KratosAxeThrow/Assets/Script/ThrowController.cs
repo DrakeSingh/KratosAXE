c296476c451b4eb52daf87ad1794fb1f9ad6d616
DrakeSingh <drakesingh1@gmail.com>
2019-05-26 18:48:07 +0530

    update

---
+++ KratosAxeThrow/Assets/Script/ThrowController.cs
@@ -0,0 +1,160 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+[RequireComponent(typeof(Animator))]
+public class ThrowController : MonoBehaviour
+{
+
+    private Animator animator;
+    private MovementInput input;
+    private Rigidbody weaponRb;
+    private WeaponAnimate weaponScript;
+    private float returnTime;
+
+    private Vector3 origLocPos;
+    private Vector3 origLocRot;
+    private Vector3 pullPosition;
+
+    [Header("Public References")]
+    public Transform weapon;
+    public Transform hand;
+    public Transform curvePoint;
+    [Space]
+    [Header("Parameters")]
+    public float throwPower = 30;
+    public float cameraZoomOffset = .3f;
+    [Space]
+    [Header("Bools")]
+    public bool walking = true;
+    public bool aiming = false;
+    public bool hasWeapon = true;
+    public bool pulling = false;
+    
+    void Start()
+    {
+        Cursor.visible = false;
+
+        animator = GetComponent<Animator>();
+        input = GetComponent<MovementInput>();
+        weaponRb = weapon.GetComponent<Rigidbody>();
+        weaponScript = weapon.GetComponent<WeaponAnimate>();
+        origLocPos = weapon.localPosition;
+        origLocRot = weapon.localEulerAngles;
+
+    }
+
+    void Update()
+    {
+
+        //If aiming rotate the player towards the camera foward, if not reset the camera rotation on the x axis
+        if (aiming)
+        {
+            input.RotateToCamera(transform);
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(Mathf.LerpAngle(transform.eulerAngles.x, 0, .2f), transform.eulerAngles.y, transform.eulerAngles.z);
+        }
+
+        //Animation States
+        animator.SetBool("Pulling", pulling);
+        walking = input.Speed > 0;
+        animator.SetBool("isWalking", walking);
+
+
+        if(Input.GetMouseButtonDown(1) && hasWeapon)
+        {
+            aiming = true;
+            animator.SetBool("Aiming", aiming);
+        }
+
+        if(Input.GetMouseButtonUp(1) && hasWeapon)
+        {
+            aiming = false;
+            animator.SetBool("Aiming", aiming);
+        }
+
+        if (hasWeapon)
+        {
+
+            if (aiming && Input.GetMouseButtonDown(0))
+            {
+                animator.SetTrigger("throw");
+            }
+
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                WeaponStartPull();
+            }
+        }
+
+        if (pulling)
+        {
+            if(returnTime < 1)
+            {
+                weapon.position = GetQuadraticCurvePoint(returnTime, pullPosition, curvePoint.position, hand.position);
+                returnTime += Time.deltaTime * 1.5f;
+            }
+            else
+            {
+                WeaponCatch();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    
+
+    public void WeaponThrow()
+    {
+
+        hasWeapon = false;
+        weaponScript.activated = true;
+        weaponRb.isKinematic = false;
+        weaponRb.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        weapon.parent = null;
+        weapon.eulerAngles = new Vector3(0, -90 +transform.eulerAngles.y, 0);
+        weapon.transform.position += transform.right/5;
+        weaponRb.AddForce(Camera.main.transform.forward * throwPower + transform.up * 2, ForceMode.Impulse);
+
+    }
+
+    public void WeaponStartPull()
+    {
+        pullPosition = weapon.position;
+        weaponRb.Sleep();
+        weaponRb.collisionDetectionMode = CollisionDetectionMode.ContinuousSpeculative;
+        weaponRb.isKinematic = true;
+        weaponScript.activated = true;
+        pulling = true;
+    }
+
+    public void WeaponCatch()
+    {
+        returnTime = 0;
+        pulling = false;
+        weapon.parent = hand;
+        weaponScript.activated = false;
+        weapon.localEulerAngles = origLocRot;
+        weapon.localPosition = origLocPos;
+        hasWeapon = true;
+
+    }
+
+    public Vector3 GetQuadraticCurvePoint(float t, Vector3 p0, Vector3 p1, Vector3 p2)
+    {
+        float u = 1 - t;
+        float tt = t * t;
+        float uu = u * u;
+        return (uu * p0) + (2 * u * t * p1) + (tt * p2);
+    }
+}
---
+++ KratosAxeThrow/Assets/Script/WeaponAnimate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponAnimate : MonoBehaviour
+{
+    public bool activated;
+    public float rotationSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activated)
+        {
+            transform.localEulerAngles += Vector3.forward * rotationSpeed * Time.deltaTime;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+        activated = false;
+    }
+}
